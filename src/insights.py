8b8396bf88908d79b1e1c97ec1154633182c3165
lucas-paz <lucas.hpaz@outlook.com>
2022-01-02 10:26:55 -0300

    req 3

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,14 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    data = jobs.read(path)
 
-    Must call `read`
+    industries = set()
+    for job in data:
+        if job["job_type"]:
+            industries.add(job["job_type"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return industries
 
 
 def filter_by_industry(jobs, industry):
